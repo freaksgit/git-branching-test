941045c7f4576dd79745a2d784f6fed2d686eb66
Vasyl Stoliarchuk <vasyl.stoliarchuk@uptech.team>
2023-07-14 09:44:59 +0300

    feature ab: commit 1

--- app/src/main/java/com/example/gitsample/MainActivity.kt
+++ app/src/main/java/com/example/gitsample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gitsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -11,6 +12,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gitsample.ui.theme.GitSampleTheme
 
@@ -27,6 +29,11 @@ class MainActivity : ComponentActivity() {
                     Column(modifier = Modifier.fillMaxSize()) {
                         Greeting("Android")
                         GoButton(text = "Go", onClick = {})
+
+                        Image(
+                            painter = painterResource(id = R.mipmap.ic_launcher),
+                            contentDescription = ""
+                        )
                     }
                 }
             }
